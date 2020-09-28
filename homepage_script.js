681a4be7cecb83411e2137125578e3342c4246f8
Bagas Adi Firdaus <bagasadifirdaus@gmail.com>
2020-09-28 23:25:58 +0700

    Pop up sign-up

--- homepage_script.js
+++ homepage_script.js
@@ -7,3 +7,13 @@ function openNav() {
     document.getElementById("Sidebar").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
   }
+
+  var newwindow;
+  function poptastic(url)
+  {
+    newwindow=window.open(url,'name','height=400,width=200');
+    if (window.focus) {newwindow.focus()}
+  }
+
+
+ 
