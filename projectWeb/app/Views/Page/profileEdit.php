83c00aa7ff0d7413f6a40d9c6cbe6882f89d2bfc
bagasadif <bagasadifirdaus@gmail.com>
2020-11-24 11:33:36 +0700

    Benerin CSS Profile Edit

--- projectWeb/app/Views/Page/profileEdit.php
+++ projectWeb/app/Views/Page/profileEdit.php
@@ -4,14 +4,14 @@
 <head>
     <!-- Bootstrap for grid -->
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
-    <link rel="stylesheet" href="../css/profileEdit.css">
+    <link rel="stylesheet" href="/css/profileEdit.css">
     <link href="https://fonts.googleapis.com/css2?family=Asap&family=Open+Sans&family=Righteous&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Karla&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Lato&family=Montserrat&display=swap" rel="stylesheet">
-    <link rel="shortcut icon" href="../images/favicon.ico" />
+    <link rel="shortcut icon" href="/images/favicon.ico" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="../js/homepage_script.js"></script>
+    <script src="/js/homepage_script.js"></script>
     <title>HAiYU</title>
 </head>
 
@@ -26,25 +26,25 @@
                 </form>
             </div>
         </div>
-        <span><img src="../images/avatar.png" alt="" id="avatar"></span>
+        <span><img src="/images/avatar.png" alt="" id="avatar"></span>
         <div class="user-info">
             <p id="welcome">Welcome! <br> <?= session('username') ?></p>
         </div>
         <div class="navUser">
             <div class="home">
-                <img src="../images/homepage.png" alt="">
+                <img src="/images/homepage.png" alt="">
                 <a href="/">Home</a>
             </div>
             <div class="subject">
-                <img src="../images/language.png" alt="">
+                <img src="/images/language.png" alt="">
                 <a href="/user/subject">Subject</a>
             </div>
             <div class="profile">
-                <img src="../images/user.png" alt="">
+                <img src="/images/user.png" alt="">
                 <a href="#">Profile</a>
             </div>
             <div class="aboutUser">
-                <img src="../images/about.png" alt="" style="width: 32px;height: 32px">
+                <img src="/images/about.png" alt="" style="width: 32px;height: 32px">
                 <a href="about">About</a>
             </div>
         </div>
@@ -55,7 +55,7 @@
     </div>
     <div class="main">
         <div class="menu">
-            <a class="openbtn" onclick="openNav()"><img src="../images/open-menu.png" alt="" style="width: 32px;height: auto"></a>
+            <a class="openbtn" onclick="openNav()"><img src="/images/open-menu.png" alt="" style="width: 32px;height: auto"></a>
             <span class="haiyu">HAiYU</span>
         </div>
     </div>
@@ -67,7 +67,7 @@
                 <div class="col">
                     <div class="form-control1">
                         <label for="imgProfile">
-                            <img src="../images/avatar.png" alt="">
+                            <img src="/images/avatar.png" alt="">
                         </label>
                         <input type="file" id="imgProfile">
                     </div>
@@ -75,9 +75,9 @@
             </div>
             <div class="row2">
                 <div class="col">
-                    <div class="form-control">
-                        <input type="text" name="id" id="id" value="<?= $user->id; ?>" readonly><br>
-                    </div>
+                    <!-- <div class="form-control">
+                        <input type="hidden" name="id" id="id" value=" $user->id; " readonly><br>
+                    </div> -->
                     <div class="form-control">
                         <input type="text" name="email" id="email" value="<?= $user->email; ?>" readonly><br>
                     </div>
@@ -95,7 +95,7 @@
                         </select><br>
                     </div>
                     <div class="form-control">
-                        <input type="date" name="birthdate" id="birthdate" value="<?= $user->birthdate; ?>">
+                        <input type="date" name="birthdate" id="birthdate" value="<?= $user->tanggal_lahir; ?>">
                     </div>
                 </div>
             </div>
